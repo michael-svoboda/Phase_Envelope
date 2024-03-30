111ed7ea071eb79f0e79966ccd817a13ae4420da
michael-svoboda <michael.svoboda1@ucalgary.ca>
2024-03-30 14:29:10 -0600

    update

--- server/start.py
+++ server/start.py
@@ -4,12 +4,21 @@ from app.routes import composition_bp
 from app.routes import phase_fraction_bp
 from app.routes.composition_route import calculate_phase_envelope
 
+
+
 app = Flask(__name__)
 CORS(app)  # Apply CORS to your Flask app
+
 
 # Register the blueprint
 app.register_blueprint(composition_bp)
 app.register_blueprint(phase_fraction_bp)
 
+@app.route('/api')
+
+def test():
+    return 'Test endpoint working!'
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
